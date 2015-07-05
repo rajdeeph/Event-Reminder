ce77022e5fd2bb0cc68d8b6b783aa9dc3f0dd0d9
rajdeeph <rajdeep.hazarika@gmail.com>
2015-07-05 21:08:21 +1000

    Update query.php

--- query.php
+++ query.php
@@ -1,58 +1,18 @@
-<<<<<<< HEAD
 <?php
-
 require_once dirname(__FILE__).'/dbconnect.php';
-
-
 class query
     {
-
         function getAllBirthdays()
         {
-
         $result = array();
         $conn = new dbconnect();
         $conn->setHandler()->query("SELECT * FROM Person");
         var_dump($conn);
         //$a = $conn->handler;
         //$result = $conn->handler->query("SELECT * FROM Person");
-
         return $result;
-
         }
-
-
     }
-
 $db = new query();
 $days = $db->getAllBirthdays();
-=======
-<?php
-
-require_once dirname(__FILE__).'/dbconnect.php';
-
-
-class query
-    {
-
-        function getAllBirthdays()
-        {
-
-        $result = array();
-        $conn = new dbconnect();
-        $conn->setHandler()->query("SELECT * FROM Person");
-        var_dump($conn);
-        //$a = $conn->handler;
-        //$result = $conn->handler->query("SELECT * FROM Person");
-
-        return $result;
-
-        }
-
-
-    }
-
-$db = new query();
-$days = $db->getAllBirthdays();
->>>>>>> 20e7a5bad43e3bcd54b0e869dde24008f1058199
 print_r($days);
